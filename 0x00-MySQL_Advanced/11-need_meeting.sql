aa6f6cd125117d069177fe1f2757bf863ca60d57
Serinajefthas <serina.jefthas@gmail.com>
2024-01-18 22:46:51 +0000

    add if exists clause for tbl

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,5 @@
 -- Create view to list all students with score < 80
-CREATE VIEW need_meeting AS
+CREATE OR REPLACE VIEW need_meeting AS
 SELECT name
 FROM students
 WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL -1 MONTH));
