febb0f44fb660d78b993695b8cbcf3b5806a8f0d
Serinajefthas <serina.jefthas@gmail.com>
2024-01-19 12:12:49 +0000

    add task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,12 @@
+-- create function that computes number dividsion
+DELIMITER $$;
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS INT
+BEGIN
+	IF b = 0 THEN
+		RETURN 0;
+	ELSE
+		RETURN a / b;
+	END IF;
+END $$;
+DELIMITER ;
