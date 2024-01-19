4166f3eaeea25c3f705a33c38a0339647e552807
Serinajefthas <serina.jefthas@gmail.com>
2024-01-19 14:53:20 +0000

    fix task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,10 +1,12 @@
 -- Create trigger to reset attribute when email is updated
 DELIMITER $$;
-CREATE TRIGGER reset_email BEFORE UPDATE ON users
+CREATE TRIGGER reset_email
+BEFORE UPDATE
+ON users
 FOR EACH ROW
 BEGIN
 	IF NEW.email != OLD.email THEN
-		SET NEW.valid_email = NULL;
+		SET NEW.valid_email = 0;
 	END IF;
 END;$$
 DELIMITER ;
