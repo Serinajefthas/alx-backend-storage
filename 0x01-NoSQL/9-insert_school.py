c7f80150d5d79ebab7e1df3d1c88a883a159ca77
Serinajefthas <serina.jefthas@gmail.com>
2024-01-23 14:41:48 -0800

    start task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Python function for NoSQL query in MongoDB"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts new doc into collection from kwargs"""
+    mongo_collection.insert({'
+
+    return _id
