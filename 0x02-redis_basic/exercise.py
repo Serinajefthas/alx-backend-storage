eca2e26a46f15523d94cdeef2380d6e55b805dd3
Serinajefthas <serina.jefthas@gmail.com>
2024-01-25 22:33:58 +0000

    add task 1 methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,3 +17,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, func: Callable[[bytes], Union[str, bytes, int, float]] = None) -> Union[str, bytes, int, float, None]:
+        """get value associated w key from redis"""
+        data = self._redis.get(key)
+        if data is not None and func is not None:
+            return func(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """gets string value associated w key from redis"""
+        return self.get(key, func = lambda x: x.decode("utf-8") if isinstance(x, bytes) else x)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """gets int value associated w key from redis"""
+        return self.get(key, func = lambda x: int(x) if isinstance(x, bytes) else x)
